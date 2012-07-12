8d3a96a76fdba4f7348951e0a662f064a48225de
Svilen Vassilev <svilen@rubystudio.net>
2012-07-12 04:26:29 +0300

    tempfiles

--- lib/vatsim_online.rb
+++ lib/vatsim_online.rb
@@ -1,5 +1,47 @@
 require "vatsim_online/version"
+require "curb"
+require "csv"
+require 'tempfile'
+require "tmpdir"
 
 module VatsimOnline
-  # Your code goes here...
+
+private
+
+  def self.status_file
+    Curl::Easy.perform("http://status.vatsim.net/status.txt").body_str
+  end
+
+  def self.create_status_tempfile
+    status = Tempfile.new('vatsim_status')
+    status.write(self.status_file)
+    status.rewind
+    File.rename status.path, "#{Dir.tmpdir}/vatsim_status.txt"
+    status.read
+  end
+
+  def self.read_status_tempfile
+    status = File.open("#{Dir.tmpdir}/vatsim_status.txt")
+    status.read
+  end
+
+  def self.servers
+    ["http://www.net-flyer.net/DataFeed/vatsim-data.txt",
+    "http://www.klain.net/sidata/vatsim-data.txt",
+    "http://fsproshop.com/servinfo/vatsim-data.txt",
+    "http://info.vroute.net/vatsim-data.txt",
+    "http://data.vattastic.com/vatsim-data.txt"]
+  end
+
+  def self.pick_random_server
+    self.servers.sample
+  end
+
+  def self.data_file
+    Curl::Easy.perform(self.pick_random_server).body_str
+  end
+
+  def self.csv_data
+    CSV.new(self.data_file, :col_sep =>':', :row_sep =>:auto)
+  end
 end
--- spec/vatsim_online_spec.rb
+++ spec/vatsim_online_spec.rb
@@ -1,1 +1,48 @@
 require 'spec_helper.rb'
+
+data = VatsimOnline.data_file
+
+describe VatsimOnline do
+
+  describe "servers" do
+    it "should contain an array of server URLs" do
+      VatsimOnline.servers.class.should eq(Array)
+      VatsimOnline.servers.size.should eq(5)
+    end
+  end
+
+  describe "pick_random_server" do
+    it "should return a string with an URL" do
+      VatsimOnline.pick_random_server.class.should eq(String)
+      VatsimOnline.pick_random_server[0..6].should eq("http://")
+    end
+  end
+
+  describe "data_file" do
+    it "should contain the right text" do
+      data[0..11].should eq("; Created at")
+    end
+  end
+
+  describe "csv_data" do
+    it "should convert to a valid CSV" do
+      VatsimOnline.csv_data.class.should eq(CSV)
+    end
+  end
+
+  # describe "create_status_tempfile" do
+  #   it "should return a status.txt contents" do
+  #     VatsimOnline.create_status_tempfile.class.should eq(String)
+  #     VatsimOnline.create_status_tempfile[0..16].should eq("; IMPORTANT NOTE:")
+  #   end
+  # end
+
+  describe "read_status_tempfile" do
+    it "should return a status.txt contents" do
+      VatsimOnline.read_status_tempfile.class.should eq(String)
+      VatsimOnline.read_status_tempfile[0..16].should eq("; IMPORTANT NOTE:")
+    end
+  end
+
+
+end
