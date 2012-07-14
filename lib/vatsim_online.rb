3b13c6401ad2d2f36a4994dcddcbbde6542647fb
Svilen Vassilev <svilen@rubystudio.net>
2012-07-14 19:23:05 +0300

    public methods

--- lib/vatsim_online.rb
+++ lib/vatsim_online.rb
@@ -1,6 +1,16 @@
 %w{curb csv tempfile tmpdir time_diff vatsim_online/version vatsim_online/station}.each { |lib| require lib }
 
+class String
+  def vatsim_online(args={})
+    VatsimOnline.vatsim_online(self, args)
+  end
+end
+
 module VatsimOnline
+
+  def self.vatsim_online(icao, args)
+  end
+
 
 private
 
@@ -42,17 +52,37 @@ private
     Time.diff(data.ctime, Time.now)[:minute] > 3 ? self.create_local_data_file : data.read
   end
 
-  def self.stations(icao)
+  def self.stations(icao, role = "all")
     stations = []
     CSV.foreach(self.data_file, :col_sep =>':', :row_sep =>:auto, encoding: "iso-8859-15") do |row|
-      callsign = row[0]
-      stations << row if callsign.to_s[0..get_query_length(icao)] == icao
+      callsign = row[0]; origin = row[11]; destination = row[13]; client = row[3]
+      stations << row if (callsign.to_s[0..get_query_length(icao)] == icao && client == "ATC") unless role == "pilot"
+      stations << row if (origin.to_s[0..get_query_length(icao)] == icao || destination.to_s[0..get_query_length(icao)] == icao) unless role == "atc"
     end
     return stations
   end
 
   def self.get_query_length(icao)
     icao.length - 1
+  end
+
+  def self.station_objects(icao, role = "all")
+    station_objects= []
+    self.stations(icao, role).each {|station| station_objects << VatsimOnline::Station.new(station) }
+    return station_objects
+  end
+
+  def self.sort_station_objects(icao, role = "all")
+    atc = []; pilots = []
+    self.station_objects(icao, role).each {|sobj| sobj.role == "ATC" ? atc << sobj : pilots << sobj}
+    return {:atc => atc, :pilots => pilots}
+  end
+
+  def self.determine_role(args)
+    args[:atc] == false ? role = "pilot" : role = "all"
+    args[:pilots] == false ? role = "atc" : role = role
+    role = "all" if args[:pilots] == false && args[:atc] == false
+    return role
   end
 
 end
--- spec/vatsim_online_spec.rb
+++ spec/vatsim_online_spec.rb
@@ -9,6 +9,13 @@ describe VatsimOnline do
   #   end
   # end
 
+  describe "status_file" do
+    it "should return status.txt path" do
+      VatsimOnline.status_file.class.should eq(String)
+      VatsimOnline.status_file.should include("vatsim_status.txt")
+    end
+  end
+
   describe "read_status_tempfile" do
     it "should confirm a file exists" do
       VatsimOnline.read_status_tempfile
@@ -16,12 +23,6 @@ describe VatsimOnline do
     end
   end
 
-  describe "status_file" do
-    it "should return status.txt path" do
-      VatsimOnline.status_file.class.should eq(String)
-      VatsimOnline.status_file.should include("vatsim_status.txt")
-    end
-  end
 
   describe "servers" do
     it "should contain an array of server URLs" do
@@ -37,18 +38,18 @@ describe VatsimOnline do
   #   end
   # end
 
-  describe "read_data_tempfile" do
-    it "should confirm a file exists" do
-      VatsimOnline.read_local_datafile
-      File.exists?("#{Dir.tmpdir}/vatsim_data.txt").should be true
-    end
-  end
-
   describe "data_file" do
     it "should contain file path" do
       data = VatsimOnline.data_file
       VatsimOnline.data_file.class.should eq(String)
       VatsimOnline.data_file.should include("vatsim_data.txt")
+    end
+  end
+
+  describe "read_data_tempfile" do
+    it "should confirm a file exists" do
+      VatsimOnline.read_local_datafile
+      File.exists?("#{Dir.tmpdir}/vatsim_data.txt").should be true
     end
   end
 
@@ -67,6 +68,83 @@ describe VatsimOnline do
       icao = "ZGGG"
       VatsimOnline.stations(icao).first[0].should eq("ZGGG_ATIS")
       VatsimOnline.stations(icao).class.should eq(Array)
+    end
+
+    it "should distinguish roles" do
+      gem_data_file
+      icao = "ZGGG"
+      VatsimOnline.stations(icao, role="atc").first[0].should eq("ZGGG_ATIS")
+      VatsimOnline.stations(icao, role="atc").class.should eq(Array)
+      VatsimOnline.stations(icao, role="pilot").length.should be(0)
+    end
+
+    it "should combine all stations" do
+      gem_data_file
+      icao = "LO"
+      VatsimOnline.stations(icao).first[0].should eq("ACH0838")
+      VatsimOnline.stations(icao).last[0].should eq("VKG342")
+      VatsimOnline.stations(icao).last[11].should eq("LOWI")
+      VatsimOnline.stations(icao).last[13].should eq("EKCH")
+      VatsimOnline.stations(icao).class.should eq(Array)
+      VatsimOnline.stations(icao).count.should eq(23)
+      VatsimOnline.stations(icao, role = "atc").count.should eq(2)
+      VatsimOnline.stations(icao, role = "atc").first[0].should eq("LOVV_CTR")
+    end
+  end
+
+  describe "station_objects" do
+    it "should return an array of Station objects" do
+      gem_data_file
+      icao = "LO"
+      VatsimOnline.station_objects(icao).class.should eq(Array)
+      VatsimOnline.station_objects(icao).size.should eq(23)
+      VatsimOnline.station_objects(icao, role="atc").size.should eq(2)
+      VatsimOnline.station_objects(icao, role="pilot").size.should eq(21)
+      VatsimOnline.station_objects(icao).first.class.should eq(VatsimOnline::Station)
+      VatsimOnline.station_objects(icao).first.callsign.should eq("ACH0838")
+    end
+  end
+
+  describe "sort_station_objects" do
+    it "should return an hash with arrays of Station objects" do
+      gem_data_file
+      icao = "LO"
+      VatsimOnline.sort_station_objects(icao).class.should eq(Hash)
+      VatsimOnline.sort_station_objects(icao).size.should eq(2)
+      VatsimOnline.sort_station_objects(icao)[:atc].class.should eq(Array)
+      VatsimOnline.sort_station_objects(icao)[:pilots].class.should eq(Array)
+      VatsimOnline.sort_station_objects(icao)[:pilots].size.should eq(21)
+      VatsimOnline.sort_station_objects(icao)[:atc].size.should eq(2)
+      VatsimOnline.sort_station_objects(icao)[:atc].first.class.should eq(VatsimOnline::Station)
+      VatsimOnline.sort_station_objects(icao)[:pilots].first.callsign.should eq("ACH0838")
+    end
+
+    it "should handle roles" do
+      gem_data_file
+      icao = "LO"
+      VatsimOnline.sort_station_objects(icao, role="atc").class.should eq(Hash)
+      VatsimOnline.sort_station_objects(icao, role="atc").size.should eq(2)
+      VatsimOnline.sort_station_objects(icao, role="atc")[:atc].class.should eq(Array)
+      VatsimOnline.sort_station_objects(icao, role="atc")[:pilots].class.should eq(Array)
+
+      VatsimOnline.sort_station_objects(icao, role="atc")[:pilots].size.should eq(0)
+      VatsimOnline.sort_station_objects(icao, role="atc")[:atc].size.should eq(2)
+      VatsimOnline.sort_station_objects(icao, role="pilot")[:atc].size.should eq(0)
+      VatsimOnline.sort_station_objects(icao, role="pilot")[:pilots].size.should eq(21)
+      VatsimOnline.sort_station_objects(icao, role="atc")[:atc].first.callsign.should eq("LOVV_CTR")
+    end
+  end
+
+  describe "determine role" do
+    it "should return a role" do
+      args = {:pilots => true, :atc => true}
+      VatsimOnline.determine_role(args).should eq("all")
+      args = {:pilots => true, :atc => false}
+      VatsimOnline.determine_role(args).should eq("pilot")
+      args = {:pilots => false, :atc => true}
+      VatsimOnline.determine_role(args).should eq("atc")
+      args = {:pilots => false, :atc => false}
+      VatsimOnline.determine_role(args).should eq("all")
     end
   end
 
